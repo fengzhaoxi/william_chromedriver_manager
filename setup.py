06097cff1b1a55e6c52111b2cab1742a46c0b76d
fengzx120 <260360804@qq.com>
2023-09-21 15:34:19 +0800

    修改setup

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='william_chromedriver_manager',
-    version='0.0.2',
+    version='0.0.3',
     description='根据Chrome浏览器版本下载对应的webdriver',
     author='ZhaoXi Feng',
     author_email='fengzx120@gmail.com',
-    url='https://gitee.com/fengzx120/william-chrome-driver-manager',
+    url='https://github.com/fengzhaoxi/william_chromedriver_manager',
     packages=find_packages(),
     install_requires=[
         'requests',
